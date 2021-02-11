a9aad8d9564cfc13d9b27e1ce81fe7d5708aaab9
ihyano <39277252+ihyano@users.noreply.github.com>
2021-02-11 16:46:29 -0300

    Add files via upload

---
+++ Store.go
@@ -0,0 +1,407 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package store
+
+import (
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// StoreABI is the input ABI used to generate the binding from.
+const StoreABI = "[{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"key\",\"type\":\"uint256\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"ItemSet\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"items\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"key\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"setItem\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"version\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]"
+
+// StoreBin is the compiled bytecode used for deploying new contracts.
+var StoreBin = "0x60806040526040518060400160405280600381526020017f76657200000000000000000000000000000000000000000000000000000000008152506000908051906020019061004f929190610062565b5034801561005c57600080fd5b50610166565b82805461006e90610105565b90600052602060002090601f01602090048101928261009057600085556100d7565b82601f106100a957805160ff19168380011785556100d7565b828001600101855582156100d7579182015b828111156100d65782518255916020019190600101906100bb565b5b5090506100e491906100e8565b5090565b5b808211156101015760008160009055506001016100e9565b5090565b6000600282049050600182168061011d57607f821691505b6020821081141561013157610130610137565b5b50919050565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052602260045260246000fd5b6103eb806101756000396000f3fe608060405234801561001057600080fd5b50600436106100415760003560e01c806317b795651461004657806354fd4d5014610062578063bfb231d214610080575b600080fd5b610060600480360381019061005b91906101e9565b6100b0565b005b61006a610105565b604051610077919061026d565b60405180910390f35b61009a600480360381019061009591906101c0565b610193565b6040516100a7919061028f565b60405180910390f35b8060016000848152602001908152602001600020819055507f80ad84813a4731e77265b63bb843785625a7ce769122c340d8c693e36ff2a90782826040516100f99291906102aa565b60405180910390a15050565b600080546101129061032c565b80601f016020809104026020016040519081016040528092919081815260200182805461013e9061032c565b801561018b5780601f106101605761010080835404028352916020019161018b565b820191906000526020600020905b81548152906001019060200180831161016e57829003601f168201915b505050505081565b60016020528060005260406000206000915090505481565b6000813590506101ba8161039e565b92915050565b6000602082840312156101d257600080fd5b60006101e0848285016101ab565b91505092915050565b600080604083850312156101fc57600080fd5b600061020a858286016101ab565b925050602061021b858286016101ab565b9150509250929050565b6000610230826102d3565b61023a81856102de565b935061024a8185602086016102f9565b6102538161038d565b840191505092915050565b610267816102ef565b82525050565b600060208201905081810360008301526102878184610225565b905092915050565b60006020820190506102a4600083018461025e565b92915050565b60006040820190506102bf600083018561025e565b6102cc602083018461025e565b9392505050565b600081519050919050565b600082825260208201905092915050565b6000819050919050565b60005b838110156103175780820151818401526020810190506102fc565b83811115610326576000848401525b50505050565b6000600282049050600182168061034457607f821691505b602082108114156103585761035761035e565b5b50919050565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052602260045260246000fd5b6000601f19601f8301169050919050565b6103a7816102ef565b81146103b257600080fd5b5056fea264697066735822122072fcbf4a7d22dcb1fedb0e8c5c3cd0b7d4cadd649a2b32121862e109aad22d2964736f6c63430008010033"
+
+// DeployStore deploys a new Ethereum contract, binding an instance of Store to it.
+func DeployStore(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Store, error) {
+	parsed, err := abi.JSON(strings.NewReader(StoreABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(StoreBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Store{StoreCaller: StoreCaller{contract: contract}, StoreTransactor: StoreTransactor{contract: contract}, StoreFilterer: StoreFilterer{contract: contract}}, nil
+}
+
+// Store is an auto generated Go binding around an Ethereum contract.
+type Store struct {
+	StoreCaller     // Read-only binding to the contract
+	StoreTransactor // Write-only binding to the contract
+	StoreFilterer   // Log filterer for contract events
+}
+
+// StoreCaller is an auto generated read-only Go binding around an Ethereum contract.
+type StoreCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// StoreTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type StoreTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// StoreFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type StoreFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// StoreSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type StoreSession struct {
+	Contract     *Store            // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// StoreCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type StoreCallerSession struct {
+	Contract *StoreCaller  // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts // Call options to use throughout this session
+}
+
+// StoreTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type StoreTransactorSession struct {
+	Contract     *StoreTransactor  // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// StoreRaw is an auto generated low-level Go binding around an Ethereum contract.
+type StoreRaw struct {
+	Contract *Store // Generic contract binding to access the raw methods on
+}
+
+// StoreCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type StoreCallerRaw struct {
+	Contract *StoreCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// StoreTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type StoreTransactorRaw struct {
+	Contract *StoreTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewStore creates a new instance of Store, bound to a specific deployed contract.
+func NewStore(address common.Address, backend bind.ContractBackend) (*Store, error) {
+	contract, err := bindStore(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Store{StoreCaller: StoreCaller{contract: contract}, StoreTransactor: StoreTransactor{contract: contract}, StoreFilterer: StoreFilterer{contract: contract}}, nil
+}
+
+// NewStoreCaller creates a new read-only instance of Store, bound to a specific deployed contract.
+func NewStoreCaller(address common.Address, caller bind.ContractCaller) (*StoreCaller, error) {
+	contract, err := bindStore(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &StoreCaller{contract: contract}, nil
+}
+
+// NewStoreTransactor creates a new write-only instance of Store, bound to a specific deployed contract.
+func NewStoreTransactor(address common.Address, transactor bind.ContractTransactor) (*StoreTransactor, error) {
+	contract, err := bindStore(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &StoreTransactor{contract: contract}, nil
+}
+
+// NewStoreFilterer creates a new log filterer instance of Store, bound to a specific deployed contract.
+func NewStoreFilterer(address common.Address, filterer bind.ContractFilterer) (*StoreFilterer, error) {
+	contract, err := bindStore(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &StoreFilterer{contract: contract}, nil
+}
+
+// bindStore binds a generic wrapper to an already deployed contract.
+func bindStore(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(StoreABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Store *StoreRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Store.Contract.StoreCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Store *StoreRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Store.Contract.StoreTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Store *StoreRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Store.Contract.StoreTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Store *StoreCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Store.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Store *StoreTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Store.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Store *StoreTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Store.Contract.contract.Transact(opts, method, params...)
+}
+
+// Items is a free data retrieval call binding the contract method 0xbfb231d2.
+//
+// Solidity: function items(uint256 ) view returns(uint256)
+func (_Store *StoreCaller) Items(opts *bind.CallOpts, arg0 *big.Int) (*big.Int, error) {
+	var out []interface{}
+	err := _Store.contract.Call(opts, &out, "items", arg0)
+
+	if err != nil {
+		return *new(*big.Int), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new(*big.Int)).(**big.Int)
+
+	return out0, err
+
+}
+
+// Items is a free data retrieval call binding the contract method 0xbfb231d2.
+//
+// Solidity: function items(uint256 ) view returns(uint256)
+func (_Store *StoreSession) Items(arg0 *big.Int) (*big.Int, error) {
+	return _Store.Contract.Items(&_Store.CallOpts, arg0)
+}
+
+// Items is a free data retrieval call binding the contract method 0xbfb231d2.
+//
+// Solidity: function items(uint256 ) view returns(uint256)
+func (_Store *StoreCallerSession) Items(arg0 *big.Int) (*big.Int, error) {
+	return _Store.Contract.Items(&_Store.CallOpts, arg0)
+}
+
+// Version is a free data retrieval call binding the contract method 0x54fd4d50.
+//
+// Solidity: function version() view returns(string)
+func (_Store *StoreCaller) Version(opts *bind.CallOpts) (string, error) {
+	var out []interface{}
+	err := _Store.contract.Call(opts, &out, "version")
+
+	if err != nil {
+		return *new(string), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new(string)).(*string)
+
+	return out0, err
+
+}
+
+// Version is a free data retrieval call binding the contract method 0x54fd4d50.
+//
+// Solidity: function version() view returns(string)
+func (_Store *StoreSession) Version() (string, error) {
+	return _Store.Contract.Version(&_Store.CallOpts)
+}
+
+// Version is a free data retrieval call binding the contract method 0x54fd4d50.
+//
+// Solidity: function version() view returns(string)
+func (_Store *StoreCallerSession) Version() (string, error) {
+	return _Store.Contract.Version(&_Store.CallOpts)
+}
+
+// SetItem is a paid mutator transaction binding the contract method 0x17b79565.
+//
+// Solidity: function setItem(uint256 key, uint256 value) returns()
+func (_Store *StoreTransactor) SetItem(opts *bind.TransactOpts, key *big.Int, value *big.Int) (*types.Transaction, error) {
+	return _Store.contract.Transact(opts, "setItem", key, value)
+}
+
+// SetItem is a paid mutator transaction binding the contract method 0x17b79565.
+//
+// Solidity: function setItem(uint256 key, uint256 value) returns()
+func (_Store *StoreSession) SetItem(key *big.Int, value *big.Int) (*types.Transaction, error) {
+	return _Store.Contract.SetItem(&_Store.TransactOpts, key, value)
+}
+
+// SetItem is a paid mutator transaction binding the contract method 0x17b79565.
+//
+// Solidity: function setItem(uint256 key, uint256 value) returns()
+func (_Store *StoreTransactorSession) SetItem(key *big.Int, value *big.Int) (*types.Transaction, error) {
+	return _Store.Contract.SetItem(&_Store.TransactOpts, key, value)
+}
+
+// StoreItemSetIterator is returned from FilterItemSet and is used to iterate over the raw logs and unpacked data for ItemSet events raised by the Store contract.
+type StoreItemSetIterator struct {
+	Event *StoreItemSet // Event containing the contract specifics and raw log
+
+	contract *bind.BoundContract // Generic contract to use for unpacking event data
+	event    string              // Event name to use for unpacking event data
+
+	logs chan types.Log        // Log channel receiving the found contract events
+	sub  ethereum.Subscription // Subscription for errors, completion and termination
+	done bool                  // Whether the subscription completed delivering logs
+	fail error                 // Occurred error to stop iteration
+}
+
+// Next advances the iterator to the subsequent event, returning whether there
+// are any more events found. In case of a retrieval or parsing error, false is
+// returned and Error() can be queried for the exact failure.
+func (it *StoreItemSetIterator) Next() bool {
+	// If the iterator failed, stop iterating
+	if it.fail != nil {
+		return false
+	}
+	// If the iterator completed, deliver directly whatever's available
+	if it.done {
+		select {
+		case log := <-it.logs:
+			it.Event = new(StoreItemSet)
+			if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+				it.fail = err
+				return false
+			}
+			it.Event.Raw = log
+			return true
+
+		default:
+			return false
+		}
+	}
+	// Iterator still in progress, wait for either a data or an error event
+	select {
+	case log := <-it.logs:
+		it.Event = new(StoreItemSet)
+		if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+			it.fail = err
+			return false
+		}
+		it.Event.Raw = log
+		return true
+
+	case err := <-it.sub.Err():
+		it.done = true
+		it.fail = err
+		return it.Next()
+	}
+}
+
+// Error returns any retrieval or parsing error occurred during filtering.
+func (it *StoreItemSetIterator) Error() error {
+	return it.fail
+}
+
+// Close terminates the iteration process, releasing any pending underlying
+// resources.
+func (it *StoreItemSetIterator) Close() error {
+	it.sub.Unsubscribe()
+	return nil
+}
+
+// StoreItemSet represents a ItemSet event raised by the Store contract.
+type StoreItemSet struct {
+	Key   *big.Int
+	Value *big.Int
+	Raw   types.Log // Blockchain specific contextual infos
+}
+
+// FilterItemSet is a free log retrieval operation binding the contract event 0x80ad84813a4731e77265b63bb843785625a7ce769122c340d8c693e36ff2a907.
+//
+// Solidity: event ItemSet(uint256 key, uint256 value)
+func (_Store *StoreFilterer) FilterItemSet(opts *bind.FilterOpts) (*StoreItemSetIterator, error) {
+
+	logs, sub, err := _Store.contract.FilterLogs(opts, "ItemSet")
+	if err != nil {
+		return nil, err
+	}
+	return &StoreItemSetIterator{contract: _Store.contract, event: "ItemSet", logs: logs, sub: sub}, nil
+}
+
+// WatchItemSet is a free log subscription operation binding the contract event 0x80ad84813a4731e77265b63bb843785625a7ce769122c340d8c693e36ff2a907.
+//
+// Solidity: event ItemSet(uint256 key, uint256 value)
+func (_Store *StoreFilterer) WatchItemSet(opts *bind.WatchOpts, sink chan<- *StoreItemSet) (event.Subscription, error) {
+
+	logs, sub, err := _Store.contract.WatchLogs(opts, "ItemSet")
+	if err != nil {
+		return nil, err
+	}
+	return event.NewSubscription(func(quit <-chan struct{}) error {
+		defer sub.Unsubscribe()
+		for {
+			select {
+			case log := <-logs:
+				// New log arrived, parse the event and forward to the user
+				event := new(StoreItemSet)
+				if err := _Store.contract.UnpackLog(event, "ItemSet", log); err != nil {
+					return err
+				}
+				event.Raw = log
+
+				select {
+				case sink <- event:
+				case err := <-sub.Err():
+					return err
+				case <-quit:
+					return nil
+				}
+			case err := <-sub.Err():
+				return err
+			case <-quit:
+				return nil
+			}
+		}
+	}), nil
+}
+
+// ParseItemSet is a log parse operation binding the contract event 0x80ad84813a4731e77265b63bb843785625a7ce769122c340d8c693e36ff2a907.
+//
+// Solidity: event ItemSet(uint256 key, uint256 value)
+func (_Store *StoreFilterer) ParseItemSet(log types.Log) (*StoreItemSet, error) {
+	event := new(StoreItemSet)
+	if err := _Store.contract.UnpackLog(event, "ItemSet", log); err != nil {
+		return nil, err
+	}
+	event.Raw = log
+	return event, nil
+}
